bd4e54c4683f299af18ed1fb1168b715d145016b
sa-jya <66937376+sa-jya@users.noreply.github.com>
2020-11-08 15:59:56 +0900

    step1 - utills
    step1 - utills

---
+++ Define.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class Define {
+	public static final int KOREAN = 1001;
+	public static final int MATH = 2001;
+	public static final int DANCE = 3001;
+	
+	
+	public static final int AB_Type = 0;
+	public static final int SAB_Type = 1;
+	public static final int PF_Type = 2;
+
+}
